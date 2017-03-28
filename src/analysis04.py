69a1f6d585fe2e079a2e601dfa2490d3843c08c4
Pitágoras Alves <alves.pitagoras@gmail.com>
2017-03-28 12:50:31 -0300

    No matplotlib

--- src/analysis04.py
+++ src/analysis04.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from bokeh.plotting import figure
 from bokeh.layouts import row
 from bokeh.plotting import ColumnDataSource
