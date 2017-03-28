ea22bf578064c22933310e3deaec14beae3dc9a8
Pitágoras Alves <alves.pitagoras@gmail.com>
2017-03-28 14:28:03 -0300

    sort -> sort_values

--- src/analysis01.py
+++ src/analysis01.py
@@ -7,8 +7,8 @@ from bokeh.models import HoverTool
 from bokeh.models import Span, Label
 from bokeh.charts import output_notebook, show, Bar, output_file, BoxPlot, save
 #datasetPath = "datasets/CO2-passenger-cars-v12-less.csv"
-datasetPath = "../datasets/CO2-passenger-cars-v12-treated-less.csv"
-#datasetPath = "datasets/CO2-passenger-cars-v12-treated.csv"
+#datasetPath = "../datasets/CO2-passenger-cars-v12-treated-less.csv"
+datasetPath = "../datasets/CO2-passenger-cars-v12-treated.csv"
 #datasetPath = "datasets/CO2_passenger_cars_v12.tsv" #use separator='\t
 euCountriesPath = "../datasets/european-union-countries.csv"
 
@@ -17,7 +17,7 @@ data = pd.read_csv(datasetPath, header=0)
 f = open('../results/lowestEmitters.txt', 'w')
 f.write("\nThe lowest CO2 emitters: \n")
 count = 0
-for label, row in data.sort(['e'], ascending=True).iterrows():
+for label, row in data.sort_values('e', ascending=True).iterrows():
     f.write(row['Mk'] + " " + row['Cn'] + ": " + str(row['e']) + '\n')
     if(count == 12):
         break
@@ -27,7 +27,7 @@ f.close()
 f = open('../results/biggestEmitters.txt', 'w')
 f.write("The biggest CO2 emitters: \n")
 count = 0
-for label, row in data.sort(['e'], ascending=False).iterrows():
+for label, row in data.sort_values('e', ascending=False).iterrows():
     f.write(row['Mk'] + " " + row['Cn'] + ": " + str(row['e'])+'\n')
     if(count == 12):
         break
